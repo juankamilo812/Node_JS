7e68d39f305e2d9178930de2c29add0d8937c3b0
Juan Camilo Garcia Hurtado <juan.garciah@arus.com.co>
2019-12-13 11:24:09 -0500

    update

--- W3school_JS/python/tuples.py
+++ W3school_JS/python/tuples.py
@@ -1,3 +1,20 @@
-#Tuple
+# #Tuple
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple)
+
+# #Access Tuple Items
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[1])
+
+# #Negative Indexing
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[-1])
+
+#Negative Indexing
+
 thistuple = ("apple", "banana", "cherry")
-print(thistuple)
+print(thistuple[-1])
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
